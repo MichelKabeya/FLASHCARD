0b3bfb7b97b2a2e97e7ff3855f74c1a078e7a768
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-04-21 14:51:58 +0200

    added the button myth and hack

--- app/src/main/java/za/ac/iie/myflashcard/Card.kt
+++ app/src/main/java/za/ac/iie/myflashcard/Card.kt
@@ -1,6 +1,8 @@
 package za.ac.iie.myflashcard
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,30 @@ class Card : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_card)
+
+         // added a background image for activity_card
+        // from: https://i.pinimg.com/736x/0b/dd/1d/0bdd1d294cf71b2d2c745785bf2a899d.jpg
+
+        // created a variable that holds the Help button
+        val help = findViewById<Button>(R.id.helpBtn)
+        help.setOnClickListener {
+            // message that explain how to play on the flashcard app
+            Toast.makeText(
+                this@Card,
+                "MYTH OR HACK\n" +
+                        "\n" +
+                        "Tap \"X\" for Myth  \n" +
+                        "Tap \"O\" for Hack  \n" +
+                        "\n" +
+                        "Put your common sense to the test.",
+                Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/za/ac/iie/myflashcard/MainActivity.kt
+++ app/src/main/java/za/ac/iie/myflashcard/MainActivity.kt
@@ -1,5 +1,6 @@
 package za.ac.iie.myflashcard
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -13,7 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+// modified the app icon background image
+// from: https://i.pinimg.com/1200x/a5/2d/d3/a52dd319e9f585b0c66f32696b520c93.jpg
 
+// added background image for activity_main the welcome activity
+// from: https://i.pinimg.com/1200x/01/3c/a0/013ca0a8b6959c25a5a2934af5dd1ef9.jpg
+
+        // created a variable that holds the  start button
+        // that display a toast message when clicked
         val info = findViewById<Button>(R.id.infoBtn)
         info.setOnClickListener {
             Toast.makeText(this@MainActivity,
@@ -22,6 +30,13 @@ class MainActivity : AppCompatActivity() {
                         "Version 1.0 — 2026",
                 Toast.LENGTH_LONG ).show()
         }
+        // created a variable that holds the explicit intent for the class Card
+        val btnGO = findViewById<Button>(R.id.startBtn)
+        btnGO.setOnClickListener {
+        val intent = Intent(this, Card::class.java)
+            startActivity(intent)
+        }
+
 
 
 
