814d7bfb762bceecda736ea78983be9b38ed9c48
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-04-21 13:07:48 +0200

    created card activity

--- app/src/main/java/za/ac/iie/myflashcard/Card.kt
+++ app/src/main/java/za/ac/iie/myflashcard/Card.kt
@@ -1,37 +1,16 @@
 package za.ac.iie.myflashcard
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Card : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val info = findViewById<Button>(R.id.infoBtn)
-        info.setOnClickListener {
-            Toast.makeText(this@MainActivity,
-                "MYTH OR HACK\n" +
-                        "MK Corporation\n" +
-                        "Version 1.0 — 2026",
-                Toast.LENGTH_LONG ).show()
-        }
-
-
-
-
-
-
-
-
-
-
+        setContentView(R.layout.activity_card)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
